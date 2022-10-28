fa879510db380042d812df21ce4f7b82849c4cff
Mikael Pettersson <evanion@icloud.com>
2022-10-28 16:22:06 +0200

    add import sorting for sanity

--- libs/eslint-config/index.js
+++ libs/eslint-config/index.js
@@ -1,7 +1,46 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
+  plugins: ["simple-import-sort", "import"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "import/export": "off",
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // `react` first, `next` second, then packages starting with a character
+          [
+            "^react$",
+            "^next",
+            "^[a-z]",
+            // All monorepo internal packages
+            "^@krubb",
+            // Packages starting with `@`
+            "^@",
+            // Packages starting with `~`
+            "^~",
+          ],
+          // Imports starting with `../`
+          [
+            "^\\.\\.(?!/?$)",
+            "^\\.\\./?$",
+            // Imports starting with `./`
+            "^\\./(?=.*/)(?!/?$)",
+            "^\\.(?!/?$)",
+            "^\\./?$",
+
+            // Side effect imports
+            "^\\u0000",
+          ],
+          // Style imports
+          ["^.+\\.s?css$"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
   },
 };
