a58b34049375e8945678f55882dcd6e016cd6172
Mikael Pettersson <evanion@icloud.com>
2022-10-28 14:38:31 +0200

    added missing config for working docker

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,8 +1,11 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   output: "standalone",
   poweredByHeader: false,
   experimental: {
     transpilePackages: ["@krubb/ui"],
+    outputFileTracingRoot: path.join(__dirname, "../../"),
   },
 };
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
   output: "standalone",
   poweredByHeader: false,
   experimental: {
     transpilePackages: ["@krubb/ui"],
+    outputFileTracingRoot: path.join(__dirname, "../../"),
     appDir: true,
   },
 };
